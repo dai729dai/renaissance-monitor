6b8b7ea8fccd6c7f8f9e87768c57e147159f3cf9
dai729dai <d.ikegami0927@gmail.com>
2026-05-31 21:14:02 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,14 +1,17 @@
 import requests
-import os
+from bs4 import BeautifulSoup
 
-webhook = os.environ["DISCORD_WEBHOOK"]
+url = "https://auctions.yahoo.co.jp/search/search?aq=-1&auccat=&ei=utf-8&fr=auc_top&oq=&p=%E3%83%AB%E3%83%8D%E3%82%B5%E3%83%B3%E3%82%B9%20%E6%A0%AA%E4%B8%BB%E5%84%AA%E5%BE%85&sc_i=&tab_ex=commerce"
 
-response = requests.post(
-    webhook,
-    json={
-        "content": "GitHub Monitor Test"
-    },
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+html = requests.get(
+    url,
+    headers=headers,
     timeout=30
 )
 
-print("Status:", response.status_code)
+print("status:", html.status_code)
+print(html.text[:500])
