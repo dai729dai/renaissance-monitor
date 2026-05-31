036bbea04119665dc2e5760a90a05699d003b240
dai729dai <d.ikegami0927@gmail.com>
2026-05-31 20:49:34 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,14 @@
-print("Hello GitHub Actions")
+import requests
+import os
+
+webhook = os.environ["DISCORD_WEBHOOK"]
+
+response = requests.post(
+    webhook,
+    json={
+        "content": "GitHub Monitor Test"
+    },
+    timeout=30
+)
+
+print("Status:", response.status_code)
